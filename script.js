f43f6ab1d8666bb53399c0a50c994fad0b51a6b9
Majiid-maker <139532352+Majiid-maker@users.noreply.github.com>
2025-03-13 23:52:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,55 +4,106 @@ const questionTitle = document.getElementById('questionTitle');
 
 const quizQuestions = [
     {
-        question: "Mana club liga jerman?",
-        image: "images/cat.png",
+        question: "Manakah foto pacar dari orang di bawah ini?",
+        image: "images/202401.jpeg",
         answers: {
-            a: { text: "Real Madrid", image: "images/rm.png" },
-            b: { text: "Bayern Munchen", image: "images/bayern.png" },
-            c: { text: "Real Betis", image: "images/rb.png" },
-            d: { text: "Barcelona", image: "images/fbc.png" }
+            a: { text: "Majiid", image: "images/wolg.jpeg" },
+            b: { text: "Jepri", image: "images/jefri.jpg" },
+            c: { text: "Angga", image: "images/angga.jpg" },
+            d: { text: "Iqbal", image: "images/iqbal.jpg" }
         },
         correctAnswer: "b"
     },
     {
-        question: "Manakah foto Majiid papi smaga?",
-        image: "images/cat.png",
+        question: "Seberapa Cantik Dara?",
         answers: {
-            a: { text: "Kucing", image: "images/gambar1.jpg" },
-            b: { text: "Anjing", image: "images/gambar2.jpg" }
+            a: "cantik",
+            b: "cantik bgt",
+            c: "cuantiik pol",
+            d: "cuaaantiiikk bgttt pol pol"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Berapa jarak tanggal lahir kita",
+        answers: {
+            a: "27",
+            b: "28",
+            c: "29",
+            d: "24"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Siapa 2 teman majiid yang aku ajak waktu kamu hss",
+        answers: {
+            a: "Jeki dan Satya",
+            b: "Satya dan Jojo",
+            c: "Ahmad dan Leo",
+            d: "Raka dan Jeki"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Siapa teman kelas majiid yang tidak ikut makan di ws waktu ultah dara yang ke 18",
+        answers: {
+            a: "Ahmad",
+            b: "Catur",
+            c: "Jojo",
+            d: "Leo"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Di negara manakah kamu saat usia 19 tahun?",
+        answers: {
+            a: "Meksiko",
+            b: "Arab",
+            c: "Indonesia",
+            d: "Australia"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Apa doorprize hss yang bikin peserta undangan pada heboh?",
+        answers: {
+            a: "Sepeda Listrik",
+            b: "Motor",
+            c: "Rumah",
+            d: "HP"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Siapakah yang dapet doorprize tersebut?",
+        answers: {
+            a: "Nasywa",
+            b: "Naya",
+            c: "Kiky",
+            d: "Yiesha"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Apa lagu yang kamu bawakan saat hss?",
+        answers: {
+            a: "Kala Cinta Menggoda",
+            b: "OK",
+            c: "Hampa",
+            d: "Kangen"
         },
         correctAnswer: "a"
     },
     {
-        question: "Ibu kota Indonesia adalah?",
+        question: "Apa hadiah yang aku kasih saat ultah 2020?",
         answers: {
-            a: "Jakarta",
-            b: "Bandung",
-            c: "Surabaya",
-            d: "Jogja"
+            a: "Kopi",
+            b: "Boneka",
+            c: "Oreo Mini dan Susu",
+            d: "Tas"
         },
-        correctAnswer: "a"
+        correctAnswer: "c"
     },
-    {
-        question: "Berapakah hasil dari 5 + 7?",
-        answers: {
-            a: "10",
-            b: "12",
-            c: "14",
-            d: "16"
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "Apa warna langit pada siang hari yang cerah?",
-        answers: {
-            a: "Merah",
-            b: "Biru",
-            c: "Hijau",
-            d: "Kelabu"
-        },
-        correctAnswer: "b"
-    }
 ];
 
 let currentQuestionIndex = 0;
@@ -130,7 +181,7 @@ nextButton.addEventListener('click', function () {
 
         // Periksa jawaban
         if (selectedAnswer === currentQuestion.correctAnswer) {
-            score += 20; // Tambahkan skor jika jawaban benar
+            score += 10; // Tambahkan skor jika jawaban benar
         } else {
             // Tandai jawaban yang salah
             answerButtons.forEach(button => {
